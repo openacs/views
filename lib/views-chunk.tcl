bffc4c5a1cea098d6a66831d188eede029cb51a1
Mario Aguado <maguado@innova.uned.es>
2006-07-24 11:30:18 +0000

    Separate SQL Postgres to file and modify function to select column Postgres dependence

--- lib/views-chunk.tcl
+++ lib/views-chunk.tcl
@@ -25,7 +25,7 @@ if {![info exists sortby]} {
 
 set user_id [ad_conn user_id]
 set admin_p [permission::permission_p -object_id $package_id -party_id $user_id -privilege admin]
-set package_tree_sortkey [acs_object::get_element -object_id $package_id -element tree_sortkey]
+
 
 if {!$admin_p} {
     set viewer_id $user_id
@@ -111,30 +111,7 @@ set where_clause [template::list::filter_where_clauses -name views -and]
 # concept anyway.  the partial solution is already in HEAD but we are
 # not using that.  See: http://openacs.org/forums/message-view?message_id=158875
 
-set query "
-    select t.* from
-    (
-    select o.object_id,
-           acs_object__name(o.object_id) as object_name,
-           p.first_names,
-           p.last_name,
-           v.views as total_views,
-           v.viewer_id as viewing_user_id,
-           v.last_viewed,
-           to_char(v.last_viewed, 'Mon DD, YYYY') as pretty_last_viewed
-    from acs_objects o,
-         views v,
-         persons p
-    where o.object_type = :object_type
-          and o.tree_sortkey between :package_tree_sortkey and tree_right(:package_tree_sortkey)
-          and v.object_id = o.object_id
-          and p.person_id = v.viewer_id
-    ) t
-    where true $where_clause
-    $orderby_clause
-"
-
-db_multirow -extend {fullname object_type_pretty_name viewer_id_filter_link} views views $query {
+db_multirow -extend {fullname object_type_pretty_name viewer_id_filter_link} views select_views {} {
     set object_type_pretty_name $one_object_type(pretty_name)
     set fullname "$first_names $last_name"
     if {$admin_p} {
