502873e7002d7d54576803de47fbae4819067b42
Mario Aguado <maguado@innova.uned.es>
2006-11-22 11:36:34 +0000

    Change parameters names for compatiblity

--- sql/oracle/views-packages.sql
+++ sql/oracle/views-packages.sql
@@ -16,30 +16,32 @@
 -- http://www.fsf.org/copyleft/gpl.html
 CREATE OR REPLACE PACKAGE views_view AS
  
-	FUNCTION record_view (object_id IN acs_objects.object_id%TYPE,
-							viewer_id IN views_views.viewer_id%TYPE) RETURN integer;
+	FUNCTION record_view (
+			p_object_id IN acs_objects.object_id%TYPE,
+			p_viewer_id IN views_views.viewer_id%TYPE) RETURN integer;
 	--update the view count of object_id for viewer viewer_id, returns view count
 END views_view;
 /
 CREATE OR REPLACE PACKAGE BODY views_view AS
 
-	FUNCTION record_view (object_id IN acs_objects.object_id%TYPE, 
-							viewer_id in views_views.viewer_id%TYPE) RETURN integer IS
+	FUNCTION record_view (
+			p_object_id IN acs_objects.object_id%TYPE, 
+			p_viewer_id in views_views.viewer_id%TYPE) RETURN integer IS
 	   v_count integer;
 	   views_count  views_views.views_count%TYPE;
 	BEGIN 
-    	select count(*) into v_count from views_views where object_id = object_id and viewer_id = viewer_id;
+    	select count(*) into v_count from views_views where object_id = p_object_id and viewer_id = p_viewer_id;
 
 	    if v_count = 0 then 
     	    INSERT into views_views(object_id,viewer_id) 
-        	VALUES (object_id, viewer_id);
+        	VALUES (p_object_id, p_viewer_id);
 	        views_count := 0;
     	else
-    	    select views_count into views_count from views_views where object_id = object_id and viewer_id = viewer_id;
+    	    select views_count into views_count from views_views where object_id = p_object_id and viewer_id = p_viewer_id;
 	        UPDATE views_views
     	       SET views_count = views_count + 1, last_viewed = sysdate
-        	 WHERE object_id = object_id
-	           and viewer_id = viewer_id;
+        	 WHERE object_id = p_object_id
+	           and viewer_id = p_viewer_id;
     	end if;
 	    return views_count + 1;
 	END record_view;
