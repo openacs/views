c48cc93e9866dbac513e1db245fdd40b422bd8aa
Mario Aguado <maguado@innova.uned.es>
2007-08-01 08:59:09 +0000

    Add oracle support and upgrade script from lors-oracle-port branch

---
+++ sql/oracle/views-create.sql
@@ -0,0 +1,21 @@
+-- Views 
+--
+-- Tracking and aggregating object views...
+--
+-- Copyright (C) 2007 Mario
+-- @author Mario Aguado
+-- @creation-date 2007-07-30
+--
+-- @cvs-id $Id$
+-- Adapted to Oracle from Jeff Davis <davis@xarg.net> postresql's implementation 
+-- This is free software distributed under the terms of the GNU Public
+-- License.  Full text of the license is available from the GNU Project:
+-- http://www.fsf.org/copyleft/gpl.html
+
+@@ views-datamodel.sql
+@@ views-procs.sql
+@@ views-triggers.sql
+
+
+
+
