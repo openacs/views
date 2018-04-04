6e6ab8449d2a6c5c79d1023bff64e0757b11dd86
Hector Romojaro <hector.romojaro@gmail.com>
2018-04-04 08:11:26 +0000

    Fix typos

--- sql/oracle/views-datamodel.sql
+++ sql/oracle/views-datamodel.sql
@@ -7,7 +7,7 @@
 -- @creation-date 2007-07-30
 --
 -- @cvs-id $Id$
--- Adapted to Oracle from Jeff Davis <davis@xarg.net> postresql's implementation 
+-- Adapted to Oracle from Jeff Davis <davis@xarg.net> postgresql's implementation
 -- This is free software distributed under the terms of the GNU Public
 -- License.  Full text of the license is available from the GNU Project:
 -- http://www.fsf.org/copyleft/gpl.html
